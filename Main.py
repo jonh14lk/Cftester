4cdaf86d3e3c3780919e6507007590f7a0e4edbd
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2020-04-09 10:46:47 -0300

    cpp file name via argv

--- Main.py
+++ Main.py
@@ -10,8 +10,9 @@ home = expanduser('~')
 url = 'http://www.codeforces.com/contest/'
 pos = '/problem/'
 
-code = sys.argv[1]
-problem = sys.argv[2]
+file = sys.argv[1]
+code = sys.argv[2]
+problem = sys.argv[3]
 url = url + code + pos + problem
 req = Request(url)
 response = urlopen(req)
@@ -19,7 +20,7 @@ page = response.read()
 soup = BeautifulSoup(page, 'html.parser')
 p = soup.findAll('pre')
 q = len(p)
-inde = 1
+index = 1
 
 for i in range(q):
   if(i % 2 == 0):
@@ -36,7 +37,7 @@ for i in range(q):
     In = In.replace('<br>','\n')
     In = In.replace('< br>','\n')
     In = In.split('\n')
-    pathe = '/home/joao/Área de Trabalho/Cftester/' + str(inde) + '.in'
+    pathe = '/home/joao/Área de Trabalho/Cftester/' + str(index) + '.in'
     f = open(pathe , 'w')
     for j in In:
       y = j.replace('\n', '')
@@ -44,9 +45,9 @@ for i in range(q):
         f.write(y + '\n')
     f.close()
     print('')
-    print('Test:' , inde)
-    os.system("g++ a.cpp -o a")
-    comm = "./a <"
-    comm = comm + str(inde) + '.in'
+    print('Test:' , index)
+    com = 'g++ ' + file + ' -o main' ;
+    comm = './main <' + str(index) + '.in'
+    os.system(com)
     os.system(comm)
-    inde = inde + 1
+    index = index + 1
